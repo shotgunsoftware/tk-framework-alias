de11891ae767dd7fb25d9c01246db4ac377bb37d
unknown <stacey.oue@autodesk.com>
2024-08-13 12:13:56 -0400

    Add decorator to check the client connection.
    * Raise an exception if the client is no longer connected with an
      informative message

--- python/tk_framework_alias/client/utils/decorators.py
+++ python/tk_framework_alias/client/utils/decorators.py
@@ -37,3 +37,37 @@ def check_server_result(func: Callable) -> Any:
         return result
 
     return wrapper
+
+
+def check_client_connection(func: Callable) -> Any:
+    """
+    A decorator function to check if the client is connected to the server.
+
+    It takes a function that is an AliasSocketIoClient method (or a function that passes an
+    AliasSocketIoClient object as the first argument) and returns a function that executes the
+    main function only if the client is connected to the server.
+
+    This is meant to be used to check if the client is connected to the server before executing
+    a function that requires a connection.
+
+    :param func: The main function to execute. This should be an AliasSocketIoClient method,
+        or a function that passes an AliasSocketIoClient as the first argument.
+
+    :return: The value returned by func.
+    """
+
+    @wraps(func)
+    def wrapper(client, *args, **kwargs):
+        if not client.connected:
+            raise ConnectionError(
+                (
+                    "Client disconnected from server and cannot send request. "
+                    "Client will automatically attempt to reconnect, but if "
+                    "the issue persists you can reload the Alias FPT Plugin "
+                    "from the Alias Utilities > Plug-in Manager to try and "
+                    "resolve the problem."
+                )
+            )
+        return func(client, *args, **kwargs)
+
+    return wrapper
